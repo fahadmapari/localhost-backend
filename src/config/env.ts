5047257f7e6fe57a84baf9211566a594dc8331e3
fahad mapari <btechy4@gmail.com>
2025-06-25 00:44:46 +0530

    jwt installation

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,4 @@ config({
   path: `.env.${process.env.NODE_ENV || "development.local"}`,
 });
 
-export const { PORT, DB_URI, NODE_ENV } = process.env;
+export const { PORT, DB_URI, NODE_ENV, JWT_SECRET, JWT_EXP_IN } = process.env;
