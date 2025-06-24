7ef269645ba5155e77c0f2a81da8fb9a213a5e3c
fahadmapari <btechy4@gmail.com>
2025-06-24 19:08:34 +0530

    user model added

---
+++ src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"],
+      maxlength: [50, "Name must be at most 50 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\\S+@\\S+\\.\\S+$/, "Email is invalid"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
