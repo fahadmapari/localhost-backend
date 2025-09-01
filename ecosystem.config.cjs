e8a673e9bd9ad68d7c1ede1036a23d3dd584e914
fahad mapari <btechy4@gmail.com>
2025-09-02 01:48:06 +0530

    only 1 instance for now

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "localhost-backend",
       script: "dist/app.js", // or app.js
-      instances: "max", // run workers = CPU cores
+      instances: "1", // run workers = CPU cores
       exec_mode: "cluster", // enables zero-downtime reloads
       autorestart: true,
       max_memory_restart: "300M",
